45f5064063d6aa61fa9d1e20dda8d1b9c82e59c3
Amanda <amanda@ciobanu.org>
2020-06-16 15:55:21 +0100

    basic functionality creating new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
@@ -10,4 +11,17 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to action:'index'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
